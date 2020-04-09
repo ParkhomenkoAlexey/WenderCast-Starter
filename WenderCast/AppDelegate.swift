d59c7ff22a855a3b4e331c89c1e6f2bc6bc6d40e
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-04-09 14:48:40 +0500

    Initial Commit

---
+++ WenderCast/AppDelegate.swift
@@ -0,0 +1,77 @@
+
+
+import UIKit
+import SafariServices
+import UserNotifications
+
+enum Identifiers {
+  static let viewAction = "VIEW_IDENTIFIER"
+  static let newsCategory = "NEWS_CATEGORY"
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    UITabBar.appearance().barTintColor = UIColor.themeGreenColor
+    UITabBar.appearance().tintColor = UIColor.white
+    registerForPushNotifications()
+    return true
+  }
+  
+  func registerForPushNotifications() {
+    UNUserNotificationCenter.current()
+      .requestAuthorization(options: [.alert, .sound, .badge]) {
+        [weak self] granted, error in
+          
+        print("Permission granted: \(granted)")
+        guard granted else { return }
+        self?.getNotificationSettings()
+    }
+  }
+  
+  func getNotificationSettings() {
+    UNUserNotificationCenter.current().getNotificationSettings { settings in
+      print("Notification settings: \(settings)")
+      
+      guard settings.authorizationStatus == .authorized else { return }
+      DispatchQueue.main.async {
+        UIApplication.shared.registerForRemoteNotifications()
+      }
+    }
+  }
+  
+  func application(
+    _ application: UIApplication,
+    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler:
+    @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    print(#function)
+    guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+      completionHandler(.failed)
+      return
+    }
+    NewsItem.makeNewsItem(aps)
+  }
+  
+  func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+    let token = tokenParts.joined()
+    print("Device Token: \(token)")
+  }
+
+  func application(
+    _ application: UIApplication,
+    didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("Failed to register: \(error)")
+  }
+}
+
